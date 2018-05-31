4290dba5fafb39f747c83accc713b2c0892b032c
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-05-31 13:16:29 +0100

    Allow sudo with TouchID

--- osx_bootstrap.sh
+++ osx_bootstrap.sh
@@ -8,6 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Use TouchID with sudo
+sudo echo "auth       sufficient     pam_tid.so" >> /etc/pam.d/sudo
+
 # Set computer name (as done via System Preferences → Sharing)
 sudo scutil --set ComputerName "work kaamputer"
 sudo scutil --set HostName "work kaamputer"
