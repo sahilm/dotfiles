99a7532c457b9ee0182724302ce912af1ed483f6
Sahil Muthoo <sahil.muthoo@gmail.com>
2018-10-20 14:33:35 +0100

    Upgrade to ruby 2.5.3

--- machine_bootstrap.sh
+++ machine_bootstrap.sh
@@ -68,12 +68,12 @@ fi
 ln -sf ${DIR}/default-gems $(rbenv root)/default-gems
 
 # Get Ruby on the box
-RUBY_VERSION="2.5.1"
+RUBY_VERSION="2.5.3"
 if ! rbenv versions --bare | grep -q ${RUBY_VERSION}; then
     rbenv install ${RUBY_VERSION} --keep
 fi
 
-rbenv global 2.5.1
+rbenv global 2.5.3
 
 # Update all default gems
 gem update --system
