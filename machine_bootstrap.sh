da46a8729179b9d880466346e76d0d2c80494a65
Sahil Muthoo <sahil.muthoo@gmail.com>
2019-11-28 00:22:18 +0000

    foo

--- machine_bootstrap.sh
+++ machine_bootstrap.sh
@@ -11,26 +11,26 @@ function setup_service() {
 export -f setup_service
 
 function use_brewed_bash() {
-    local brew_bash="$(brew --prefix)/bin/bash"
-    if ! grep -q ${brew_bash} /etc/shells; then
-        sudo bash -c "echo $brew_bash >> /etc/shells"
-    fi
+  local brew_bash="$(brew --prefix)/bin/bash"
+  if ! grep -q ${brew_bash} /etc/shells; then
+    sudo bash -c "echo $brew_bash >> /etc/shells"
+  fi
 
-    if ! which bash | grep -q ${brew_bash}; then
-        sudo chsh -s "$brew_bash" ${USER}
-    fi
+  if ! which bash | grep -q ${brew_bash}; then
+    sudo chsh -s "$brew_bash" ${USER}
+  fi
 }
 
 # Installs brew and keeps all brews up-to-date
 function brew_it() {
-    which brew &>/dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew tap Homebrew/bundle
-    brew update
-    brew bundle --file=$DIR/brewfile
-    brew upgrade
-    brew cask outdated --quiet | xargs -I % brew cask reinstall %
-    mas upgrade
-    brew cleanup
+  which brew &>/dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew tap Homebrew/bundle
+  brew update
+  brew bundle --file=$DIR/brewfile
+  brew upgrade
+  brew cask outdated --quiet | xargs -I % brew cask reinstall %
+  mas upgrade
+  brew cleanup
 }
 
 # Setup dotfiles
@@ -54,7 +54,7 @@ chmod 0400 $HOME/Dropbox/creds/.ssh/id_rsa
 ln -sf $HOME/Dropbox/creds/.ssh/id_rsa $HOME/.ssh/id_rsa
 ln -sf $HOME/Dropbox/.bash_history $HOME/.bash_history
 
-. $HOME/.bash_profile &> /dev/null
+. $HOME/.bash_profile &>/dev/null
 
 # Gems to install by default on any new Ruby. See rbenv-default-gems
 ln -sf ${DIR}/default-gems $(rbenv root)/default-gems
@@ -62,7 +62,7 @@ ln -sf ${DIR}/default-gems $(rbenv root)/default-gems
 # Get Ruby on the box
 RUBY_VERSION="2.6.5"
 if ! rbenv versions --bare | grep -q ${RUBY_VERSION}; then
-    rbenv install ${RUBY_VERSION} --keep
+  rbenv install ${RUBY_VERSION} --keep
 fi
 
 rbenv global 2.6.5
